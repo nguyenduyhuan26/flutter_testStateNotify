883fc160535e461d7fae490e434e1f0233820844
huannd <besonxanh26@gmail.com>
2021-10-05 14:28:29 +0700

    change file validate

--- lib/until/validate.dart
+++ lib/until/validate.dart
@@ -1,10 +1,10 @@
 String? passwordValidate(String pass) {
   if (pass.isEmpty) {
     return 'Please enter some text!';
-  } else if (pass.length <= 3) {
+  } else if (pass.length < 3) {
     return "Password must be at least 3 characters";
   } else if (pass.length >= 12)
-    return "Password must be more 12 characters";
+    return "Password must be less 12 characters";
   else
     return null;
 }
@@ -12,10 +12,10 @@ String? passwordValidate(String pass) {
 String? userNameValidate(String name) {
   if (name.isEmpty) {
     return 'Please enter some text!';
-  } else if (name.length <= 3) {
+  } else if (name.length < 3) {
     return "Username must be at least 3 characters";
   } else if (name.length >= 12)
-    return "Username must be more 12 characters";
+    return "Password must be less 12 characters";
   else
     return null;
 }
